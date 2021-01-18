e9f441d21b16aa9360f44c2b84c060086a879aa8
shwns212 <shwns212@gmail.com>
2021-01-19 01:24:37 +0900

    create project

---
+++ shop-order/src/main/kotlin/com/jun/shop/ShopOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.jun.shop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShopOrderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShopOrderApplication>(*args)
+}
